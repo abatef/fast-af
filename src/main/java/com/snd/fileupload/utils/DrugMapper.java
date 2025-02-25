53652a7625b929b01497f231651ff6db33fd0233
abatef <abdoatef.ab@gmail.com>
2025-02-25 23:47:20 +0200

    fixed DrugMapper

--- src/main/java/com/snd/fileupload/utils/DrugMapper.java
+++ src/main/java/com/snd/fileupload/utils/DrugMapper.java
@@ -17,10 +17,10 @@ public class DrugMapper {
         dto.setId(drug.getId());
         dto.setName(drug.getName());
         dto.setDescription(drug.getDescription());
-//        dto.setCreatedBy(drug.getCreatedBy().getUsername());
+        dto.setCreatedBy(drug.getCreatedBy() != null ? drug.getCreatedBy().getUsername() : null);
         dto.setCreateTime(drug.getCreatedAt());
         dto.setUpdateTime(drug.getUpdatedAt());
-        dto.setNumberOfImages(drug.getImages().size());
+        dto.setNumberOfImages(drug.getImages() != null ? drug.getImages().size() : null);
         return dto;
     }
 }

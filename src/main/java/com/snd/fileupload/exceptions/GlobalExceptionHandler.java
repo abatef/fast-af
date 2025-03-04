d4fe3649e3d842fb8b298d0dfd7a7288bf029faa
abatef <abdoatef.ab@gmail.com>
2025-03-04 16:31:39 +0200

    exception handling

---
+++ src/main/java/com/snd/fileupload/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.snd.fileupload.exceptions;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    public void handle() {
+        return;
+    }
+
+}

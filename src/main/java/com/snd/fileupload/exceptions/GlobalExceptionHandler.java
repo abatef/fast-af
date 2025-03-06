f2d5223141eaf52d1d3bf72501ad265c4705791f
abatef <abdoatef.ab@gmail.com>
2025-03-07 00:31:38 +0200

    updated the drug form

--- src/main/java/com/snd/fileupload/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/snd/fileupload/exceptions/GlobalExceptionHandler.java
@@ -1,14 +1,23 @@
 package com.snd.fileupload.exceptions;
 
+import com.snd.fileupload.dtos.DrugInfoDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = Exception.class)
-    public void handle(Exception e) {
+    public ResponseEntity<DrugInfoDto> handle(Exception e) {
         e.printStackTrace();
+        DrugInfoDto infoDto = new DrugInfoDto();
+        infoDto.setId(0);
+        infoDto.setName("عطية خول");
+        infoDto.setForm("عطية خول برضو");
+        return new ResponseEntity<>(infoDto, HttpStatus.OK);
     }
 
 }
